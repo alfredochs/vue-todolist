b7477b371e04583d057e9bcef83e9718ee251c28
alfredochs <chanta.alfredo@gmail.com>
2021-11-02 17:30:31 +0100

    push activity

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ Vue.config.devtools = true;
 new Vue({
     el: "#root",
     data: {
-
+        benvenuto: "Hello Vueoolean - ToDoList",
+        inputText: "",
+        activityList: [
+            { testo: "Fare la spesa" },
+            { testo: "Studiare" }
+        ]
+    },
+    methods: {
+        addActivity() {
+            this.activityList.push({
+                testo: this.inputText
+            });
+            this.inputText = "";
+        }
     }
 });
+
